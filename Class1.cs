2281657b46376992bbc22bf8cd0d1a39abac8491
Iro-sources <mohamediro98@gmail.com>
2025-07-07 15:00:24 +0200

    variable declaration

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class program 
+{
+	static void Main(string []args)
+	{
+        foreach (var arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+class Program
 
+{
+    static void Main()
+    {
+        string s1 = new string("older versions");
+        string s2 = "older version";
+        var s3 = "c# 3";
+        string s4 = new("C# 9");
         
+        Console.WriteLine(s1);
+        Console.WriteLine(s2);
+        Console.WriteLine(s3);
+        Console.WriteLine(s4);
+
+
     }
+
+
+  
+
+  
+
 }
