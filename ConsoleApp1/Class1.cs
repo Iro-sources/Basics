313468e545c0109e477a9a63a85b86abfa845323
Iro-sources <mohamediro98@gmail.com>
2025-07-13 01:25:51 +0200

    Switch statement

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -6,9 +6,19 @@
     /// </summary>
     public Class1()
     {
-        Program program = new Program();
-        program.s1 = "Hello from Class1!";
-        Console.WriteLine(program.s1);
+        Program p = new Program();
+        p.s1 = "Hello from Class1!";
+        Console.WriteLine(p.s1);
+
+        // Difference between variabl of class & instance of a class
+        //Program p; // p i a Variable of the class
+        //Variable of a class is a copy of the class that is not initialized. 
+
+        //p = new Program(); // p is an Instance of a class
+        //Instance of a class is the copy of the class that is initialized by using the new keyword. 
+
+        //Program p = new Program();
+        //p1 = p; //P2 is the refrence of the class, it is the pointer of p. They are allocated to the same memory address. 
     }
 }
 
